5548d1a8c692798abcae08bc9b1dbb89380cc6c7
wiz-rd <126345400+wiz-rd@users.noreply.github.com>
2024-09-18 21:37:04 +0200

    First working prototype
    Added color support functionally.
    
    What the code does, simply, is takes the selected object, duplicates and hides it, and then recolors the duplicate object based on each face's vector normal to the global z axis. Essentially, it compares the global "slope" of each face to see if Mario can stand on it. Currently, the "steepness" value is hard-coded and cannot be changed without refreshing the addon, but I hope to change this shortly. The variable right now is 45 degrees, so if a face is 45 degrees or more "turned away" from the global z axis, then it's colored red. Otherwise, it's colored green.

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ bl_info = {
     "category": "Object",
 }
 
+from math import degrees
+
 import bpy
 import bmesh
-import numpy as np
+from mathutils import Vector
 
 
 class ShowStandableGround(bpy.types.Operator):
@@ -25,30 +27,50 @@ class ShowStandableGround(bpy.types.Operator):
         return mat
 
     def execute(self, context):
-        MAX_STEEPNESS = 0.5
+        # max steepness of slope in degrees
+        # when compared to global z
+        # global rotation is counted,
+        # so faces "facing" downwards are
+        # considered "too steep"
+        MAX_STEEPNESS = 45
+
+        # the global z vector. Maybe Blender
+        # has one by default but it's not hard to add
+        # and I can't find any reference to one online
+        GLOBAL_Z = Vector((0, 0, 1))
+
         # store original object to hide later
         # (because we can't hide it and it still be selected)
         original_obj = context.active_object
+        or_name = original_obj.name
+
         # duplicate original/selected object
         bpy.ops.object.duplicate(linked=0,mode='TRANSLATION')
+
         # hide original object
         original_obj.hide_set(True)
+
         # store the new object in a variable
         new_obj = context.active_object
-        new_obj.name = "StandableGround"
+        new_obj.name = f"Stand_{or_name}"
 
-        green_mat = self.create_material("StandableGreen", (0, 1, 0, 1))
-        red_mat = self.create_material("NonstandableRed", (1, 0, 0, 1))
+        green_mat = self.create_material(f"StandableGreen_{or_name}", (0, 1, 0, 1))
+        red_mat = self.create_material(f"NonstandableRed_{or_name}", (1, 0, 0, 1))
+        # blue_mat = self.create_material(f"ProblematicBlue_{or_name}", (0, 0, 1, 1))
 
-        # if no materials exist, add them
-        if len(new_obj.materials) == 0:
-            new_obj.data.materials.append(green_mat)
-        # if only green exists, add red
-        if len(new_obj.materials) == 1:
-            new_obj.data.materials.append(red_mat)
+        # if materials already exist (on the DUPICATE
+        # OBJECT - this is non-destructive)
+        # then delete them
+        if len(new_obj.data.materials) > 0:
+            new_obj.data.materials.clear()
+
+        new_obj.data.materials.append(green_mat)
+        new_obj.data.materials.append(red_mat)
+        # new_obj.data.materials.append(blue_mat)
 
         # get the data for that object
         new_obj_mesh = new_obj.to_mesh(preserve_all_data_layers=True)
+
         # "globalize" the data. Not sure if this is necessary anymore
         # as I removed manual calculation of face normals
         new_obj_mesh.transform(new_obj.matrix_world)
@@ -64,34 +86,41 @@ class ShowStandableGround(bpy.types.Operator):
         standable_faces = set()
         not_standable_faces = set()
 
-        # get all vertices of each face
+        # get normal vectors of each face
+        # and compare them to the global z
         for face in bm.faces:
             # append to the great vertex list
             # the index and coordinates of each vertices
             # of each face
             too_steep = False
+            normal = face.normal
+            angle_difference_to_z = normal.angle(GLOBAL_Z)
+            angle_to_z_deg = degrees(angle_difference_to_z)
 
-            print(face.normal.z)
-            print(f"is too steep: {face.normal.z > MAX_STEEPNESS}")
+            if angle_to_z_deg > MAX_STEEPNESS:
+                too_steep = True
 
             if not too_steep:
-                print(f"Appended {face} to highlight list.")
                 # appending the face to the list to be highlighted
                 standable_faces.add(face.index)
             else:
+                # similar to above
                 not_standable_faces.add(face.index)
+
+        print(f"{len(standable_faces)} faces found standable.")
+        print(f"{len(not_standable_faces)} faces found non-standable.")
 
         for face in standable_faces:
             # setting face colors to green
-            new_obj.data.polygons[face].material_index = 1
+            new_obj.data.polygons[face].material_index = 0
 
         for face in not_standable_faces:
             # setting face colors to red
-            new_obj.data.polygons[face].material_index = 2
+            new_obj.data.polygons[face].material_index = 1
 
-        # bmesh.ops.delete(bm, geom=selected_faces, context="FACES")
-        # bm.to_mesh(new_obj_mesh)
-        # new_obj.update_from_editmode()
+        # for face in standable_faces U not_standable_faces:
+        #     print(f"We have a problem. Face {face} is in both sets.")
+        #     new_obj.data.polygons[face].material_index = 2
 
         return {"FINISHED"}
 
